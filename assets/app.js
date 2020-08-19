e5cb6a4f0d31ca1d14bd122337bbab0152887e13
Lokmane Daoudi <Lokmaneda01@gmail.com>
2020-08-19 19:56:04 +0100

    Connected to contact form to Firebase

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,47 @@
-$(document).ready(function() {
-    $("body").removeClass("preload");
+ // * Firebase Config
+ const firebaseConfig = {
+  apiKey: "AIzaSyCjevr64ueQTgYHrbu0nWm3e7GHqD9C8cA",
+  authDomain: "juncture-book-c0ebf.firebaseapp.com",
+  databaseURL: "https://juncture-book-c0ebf.firebaseio.com",
+  projectId: "juncture-book-c0ebf",
+  storageBucket: "juncture-book-c0ebf.appspot.com",
+  messagingSenderId: "455333212304",
+  appId: "1:455333212304:web:dcf4cf7a7e7c2ff7a02815",
+  measurementId: "G-D4Y088FR28"
+}; 
+
+// * Refrence messeages collection
+var messagesRef = firebase.database().ref("messages")
+
+// * Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// * Listen for form submit
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+// submit form
+function submitForm(e){
+   e.preventDefault();
+   //* Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  saveMessage(name, email, message);
+  }
+
+//* Function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// * Sava message
+
+function saveMessage(name,email,message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+   name: name,
+   email: email,
+   message : message
   });
+}
